1d70faa32eecbc2ad7f77828af05b4f78d912110
TaranPreet Singh Bhullar <bhullartaran0001@gmail.com>
2023-02-20 15:27:53 -0500

    2nd-com

--- blog-service.js
+++ blog-service.js
@@ -6,7 +6,7 @@ const categoriesPath = "./data/categories.json";
 let posts = [];
 let categories = [];
 
-function initialize() {
+const initialize = () => {
     return new Promise((resolve, reject) => {
         fs.readFile(postsPath, "utf8", (err, data) => {
             if (err) {
@@ -24,9 +24,9 @@ function initialize() {
             }
         });
     });
-}
+};
 
-function getAllPosts() {
+const getAllPosts = () => {
     return new Promise((resolve, reject) => {
         if (posts.length === 0) {
             reject("No posts found");
@@ -34,20 +34,20 @@ function getAllPosts() {
             resolve(posts);
         }
     });
-}
+};
 
-function getPublishedPosts() {
+const getPublishedPosts = () => {
     return new Promise((resolve, reject) => {
-        const publishedPosts = posts.filter(post => post.published);
+        const publishedPosts = posts.filter((post) => post.published);
         if (publishedPosts.length === 0) {
             reject("No published posts found");
         } else {
             resolve(publishedPosts);
         }
     });
-}
+};
 
-function getCategories() {
+const getCategories = () => {
     return new Promise((resolve, reject) => {
         if (categories.length === 0) {
             reject("No categories found");
@@ -55,11 +55,53 @@ function getCategories() {
             resolve(categories);
         }
     });
-}
+};
+
+
+
+const getPostsByCategory = (category) => {
+    return new Promise((resolve, reject) => {
+        const filteredPosts = posts.filter((post) => post.category === category);
+        if (filteredPosts.length === 0) {
+            reject("No results returned");
+        } else {
+            resolve(filteredPosts);
+        }
+    });
+};
+
+const getPostsByMinDate = (minDateStr) => {
+    return new Promise((resolve, reject) => {
+        const filteredPosts = posts.filter((post) => new Date(post.postDate) >= new Date(minDateStr));
+        if (filteredPosts.length === 0) {
+            reject("No results returned");
+        } else {
+            resolve(filteredPosts);
+        }
+    });
+};
+
+const getPostById = (id) => {
+    return new Promise((resolve, reject) => {
+        const post = posts.find((post) => post.id === id);
+        if (!post) {
+            reject("No result returned");
+        } else {
+            resolve(post);
+        }
+    });
+};
+
+
+
 
 module.exports = {
     initialize,
     getAllPosts,
     getPublishedPosts,
-    getCategories
+    getCategories,
+    getPostsByCategory,
+    getPostsByMinDate,
+    getPostById
+
 };
--- server.js
+++ server.js
@@ -1,42 +1,96 @@
 const express = require("express");
 const blogService = require("./blog-service");
+
+const bodyParser = require('body-parser');
 const app = express();
+const path = require("path");
 
-app.use(express.static("public"));
-const port = process.env.PORT || 8080;
+const cloudinary = require('cloudinary').v2;
+const upload = require('./handlers/multer');
+require('./handlers/cloudinary');
 
-app.get("/", (req, res) => {
-    res.redirect("/about");
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
+
+cloudinary.config({
+    cloud_name: 'dxwgwfobi',
+    api_key: '498436643178297',
+    api_secret: 'MVMq5wnWIhBgt8HbPaA4WRJYTCU'
 });
 
-app.get("/about", (req, res) => {
-    res.sendFile(`${__dirname}/views/about.html`);
+app.get('/', (req, res) => {
+    res.redirect('/about');
 });
 
-app.get("/blog", async (req, res) => {
-    try {
-        const posts = await blogService.getPublishedPosts();
-        res.json(posts);
-    } catch (err) {
-        res.status(500).send(err);
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/posts/add', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/addPost.html'));
+});
+
+app.get("/blog", (req, res) => {
+    blogService
+        .getPublishedPosts()
+        .then((posts) => res.json(posts))
+        .catch((err) => res.status(500).send(err));
+});
+
+app.get("/posts", (req, res) => {
+    blogService
+        .getAllPosts()
+        .then((posts) => res.json(posts))
+        .catch((err) => res.status(500).send(err));
+});
+
+app.get("/categories", (req, res) => {
+    blogService
+        .getCategories()
+        .then((categories) => res.json(categories))
+        .catch((err) => res.status(500).send(err));
+});
+
+app.get("/posts", (req, res) => {
+    const category = req.query.category;
+    const minDate = req.query.minDate;
+
+    if (category) {
+        blogService
+            .getPostsByCategory(category)
+            .then((posts) => res.json(posts))
+            .catch((err) => res.status(500).send(err));
+    } else if (minDate) {
+        blogService
+            .getPostsByMinDate(minDate)
+            .then((posts) => res.json(posts))
+            .catch((err) => res.status(500).send(err));
+    } else {
+        blogService
+            .getAllPosts()
+            .then((posts) => res.json(posts))
+            .catch((err) => res.status(500).send(err));
     }
 });
 
-app.get("/posts", async (req, res) => {
-    try {
-        const posts = await blogService.getAllPosts();
-        res.json(posts);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+app.get("/post/:id", (req, res) => {
+    const id = req.params.id;
+
+    blogService
+        .getPostById(id)
+        .then((post) => res.json(post))
+        .catch((err) => res.status(500).send(err));
 });
 
-app.get("/categories", async (req, res) => {
+app.post('/posts/add', upload.single('image'), async (req, res) => {
     try {
-        const categories = await blogService.getCategories();
-        res.json(categories);
+        const result = await cloudinary.uploader.upload(req.file.path);
+        res.send(result);
     } catch (err) {
-        res.status(500).send(err);
+        console.error(err);
+        res.status(500).send('Error uploading file');
     }
 });
 
@@ -44,9 +98,17 @@ app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
 
-(async () => {
-    await blogService.initialize();
-    app.listen(port, () => {
-        console.log(`Start server on port ${port}`);
+blogService
+
+
+blogService
+    .initialize()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Express Server is starting on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
     });
-})();
+
